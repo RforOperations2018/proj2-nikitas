bbf5e2c2f1f408d9629b5c3a912bcd3fd8f57cbd
Nikita Setia <nikitas@andrew.cmu.edu>
2018-10-19 14:14:11 -0400

    it finally worked!!!

--- app.R
+++ app.R
@@ -33,10 +33,12 @@ ckanUniques <- function(id, field) {
 neighborhoods <- sort(ckanUniques("76fda9d0-69be-4dd5-8108-0de7907fc5a4", "NEIGHBORHOOD")$NEIGHBORHOOD)
 sources <- sort(ckanUniques("76fda9d0-69be-4dd5-8108-0de7907fc5a4", "REQUEST_ORIGIN")$REQUEST_ORIGIN)
 types <- sort(ckanUniques("76fda9d0-69be-4dd5-8108-0de7907fc5a4", "REQUEST_TYPE")$REQUEST_TYPE)
+departments <- sort(ckanUniques("76fda9d0-69be-4dd5-8108-0de7907fc5a4", "DEPARTMENT")$DEPARTMENT)
 dates <- sort(ckanUniques("76fda9d0-69be-4dd5-8108-0de7907fc5a4", "CREATED_ON")$CREATED_ON)
 dates <- as.data.frame(dates)
 years <- separate(dates, dates, "YEAR", '-')
 years <- unique(years$YEAR)
+
 
 #pdf(NULL)
 
@@ -50,7 +52,7 @@ sidebar <- dashboardSidebar(
     # Setting dashboard menu items / pages
     menuItem("Trends by Request Type", icon = icon("hand-o-up"), tabName = "type"),
     menuItem("Neighborhood Report", icon = icon("group"), tabName = "neighborhood"),
-    menuItem("Origin Analysis", icon = icon("mobile"), tabName = "origin"),
+    menuItem("Department Workflow", icon = icon("building-o"), tabName = "department"),
     menuItem("Data Exploration", icon = icon("table"), tabName = "data"),
     # no global inputs needed
     # Adding reset button to reset ALL filters
@@ -87,7 +89,7 @@ body <- dashboardBody(tabItems(
                                         choices = types,
                                         multiple = FALSE,
                                         selectize = TRUE,
-                                        selected = c("Pothole")),
+                                        selected = c("Potholes")),
                             plotlyOutput("type_plot"), 
                             br(),
                             br(),
@@ -151,44 +153,83 @@ ui <- dashboardPage(header, sidebar, body, skin = "green", useShinyjs())
 # Defining server logic
 server <- function(input, output, session) {
   # Creating filtered pitt 311 data
-  pittFiltered <- reactive({
+  typeFiltered <- reactive({
     # Build API Query with proper encodes
     # Building an IN selector
-    types_filter <- ifelse(length(input$type_select) > 0, 
-                           paste0("%20AND%20%22REQUEST_TYPE%22%20IN%20(%27", paste(input$type_select, collapse = "%27,%27"),"%27)"),"")
-    source_filter <- ifelse(length(input$source_select) > 0,
-                            paste0("%20AND%20%22REQUEST_ORIGIN%22%20IN%20(%27", paste(input$source_select, collapse = "%27,%27"),"%27)"),"")
-    nbhd_filter <- ifelse(length(input$source_select) > 0,
-                            paste0("%20AND%20%22NEIGHBORHOOD%22%20IN%20(%27", paste(input$source_select, collapse = "%27,%27"),"%27)"),"")
-    
-      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2276fda9d0-69be-4dd5-8108-0de7907fc5a4%22%20WHERE%20%22NEIGHBORHOOD%22%20=%20%27", input$nbhd_select, 
-                    "%27%20AND%20%22REQUEST_ORIGIN%22%20=%20%27", input$source_select, 
-                    "%27%20AND%20%22REQUEST_TYPE%22%20=%20%27", input$type_select, "%27")
+      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2276fda9d0-69be-4dd5-8108-0de7907fc5a4%22%20WHERE%20%22REQUEST_TYPE%22%20=%20%27", input$type_select, "%27")
       url <- gsub(pattern = " ", replacement = "%20", x = url)
       
     data <- ckanSQL(url)
     
     # Load and clean data
     if (is.null(data[1,1])){
-        # updateSelectInput(session, "nbhd_select", selected = "Brookline")
-        # updateSelectInput(session, "source_select",  selected = "Call Center")
-        # updateDateRangeInput(session, "date_select", start = Sys.Date()-30, end = Sys.Date())
-        alert("There is no data available for your selected inputs. Your filters have been reset. Please try again.")
+        updateSelectInput(session, "type_select", selected = "Potholes")
+        alert("There is no data available for your selected input(s). Your filters have been reset. Please try again.")
     } else {
-      data_YEAR <- data %>% separate(CREATED_ON, "YEAR", sep = '-') %>% subset(data, select = c(REQUEST_ID, YEAR))
+      data_YEAR <- data %>% separate(CREATED_ON, "YEAR", sep = '-') %>% subset(select = c(REQUEST_ID, YEAR))
       data_DATE <- data %>%
         mutate(DATE = as.Date(CREATED_ON),
                STATUS = ifelse(STATUS == 1, "Closed", "Open"))
       data <- merge(data_DATE, data_YEAR, key = "REQUEST_ID")
+      nrow(data)
+      ncol(data)
       data <- data %>% arrange(DATE)
       return(data) 
     }
     
   })
   
+  # nbhdFiltered <- reactive({
+  #   # Build API Query with proper encodes
+  #   # Building an IN selector
+  #   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2276fda9d0-69be-4dd5-8108-0de7907fc5a4%22%20WHERE%20%22NEIGHBORHOOD%22%20=%20%27", input$nbhd_select, "%27")
+  #   url <- gsub(pattern = " ", replacement = "%20", x = url)
+  #   
+  #   data <- ckanSQL(url)
+  #   
+  #   # Load and clean data
+  #   if (is.null(data[1,1])){
+  #     updateSelectInput(session, "nbhd_select", selected = "Brookline")
+  #     alert("There is no data available for your selected input(s). Your filters have been reset. Please try again.")
+  #   } else {
+  #     data_YEAR <- data %>% separate(CREATED_ON, "YEAR", sep = '-') %>% subset(data, select = c(REQUEST_ID, YEAR))
+  #     data_DATE <- data %>%
+  #       mutate(DATE = as.Date(CREATED_ON),
+  #              STATUS = ifelse(STATUS == 1, "Closed", "Open"))
+  #     data <- merge(data_DATE, data_YEAR, key = "REQUEST_ID")
+  #     data <- data %>% arrange(DATE)
+  #     return(data) 
+  #   }
+  #   
+  # })
+  # 
+  # deptFiltered <- reactive({
+  #   # Build API Query with proper encodes
+  #   # Building an IN selector  
+  #   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2276fda9d0-69be-4dd5-8108-0de7907fc5a4%22%20WHERE%20%22DEPARTMENT%22%20=%20%27", input$dept_select, "%27")
+  #   url <- gsub(pattern = " ", replacement = "%20", x = url)
+  #   
+  #   data <- ckanSQL(url)
+  #   
+  #   # Load and clean data
+  #   if (is.null(data[1,1])){
+  #     updateSelectInput(session, "dept_select", selected = "DPW")
+  #     alert("There is no data available for your selected input(s). Your filters have been reset. Please try again.")
+  #   } else {
+  #     data_YEAR <- data %>% separate(CREATED_ON, "YEAR", sep = '-') %>% subset(data, select = c(REQUEST_ID, YEAR))
+  #     data_DATE <- data %>%
+  #       mutate(DATE = as.Date(CREATED_ON),
+  #              STATUS = ifelse(STATUS == 1, "Closed", "Open"))
+  #     data <- merge(data_DATE, data_YEAR, key = "REQUEST_ID")
+  #     data <- data %>% arrange(DATE)
+  #     return(data) 
+  #   }
+  #   
+  # })
+  
    # Line graph showing count of all requests over time
   output$count_plot <- renderPlotly({
-    dat <- pittFiltered() %>% group_by(YEAR) %>% summarise(COUNT = n())
+    dat <- typeFiltered() %>% group_by(YEAR) %>% summarise(COUNT = n())
     ggplotly(
       ggplot(data = dat, aes(x = YEAR, y = COUNT)) + 
         geom_histogram(stat = "identity") + 
@@ -198,7 +239,7 @@ server <- function(input, output, session) {
     })
   # Point and smooth graph showing count of requests by type over time
   output$type_plot <- renderPlotly({
-    dat <- pittFiltered() %>% group_by(REQUEST_TYPE, DATE) %>% summarise(COUNT = n())
+    dat <- typeFiltered() %>% group_by(REQUEST_TYPE, DATE) %>% summarise(COUNT = n())
     ggplotly(
       ggplot(data = dat, aes(x = DATE, y = COUNT, color = REQUEST_TYPE)) +
         geom_point() + geom_smooth() + 
@@ -208,7 +249,7 @@ server <- function(input, output, session) {
     })
    # Bar chart showing count by status for each request
    output$status_plot <- renderPlotly({
-     dat <- pittFiltered() %>% group_by(REQUEST_TYPE, STATUS) %>% summarise(COUNT = n())
+     dat <- typeFiltered() %>% group_by(REQUEST_TYPE, STATUS) %>% summarise(COUNT = n())
      ggplotly(
        ggplot(data = dat, aes(x = STATUS, y = COUNT, fill = REQUEST_TYPE)) +
          geom_bar(stat = "identity") + 
--- trial_and_error.R
+++ trial_and_error.R
@@ -16,4 +16,14 @@ dates <- as.data.frame(dates)
 years <- separate(dates, dates, "year", '-')
 years <- unique(years$year)
 
+types_filter <- ifelse(length(input$type_select) > 0, 
+                       paste0("%20AND%20%22REQUEST_TYPE%22%20IN%20(%27", paste(input$type_select, collapse = "%27,%27"),"%27)"),"")
+
+
+nbhd_filter <- ifelse(length(input$source_select) > 0,
+                      paste0("%20AND%20%22NEIGHBORHOOD%22%20IN%20(%27", paste(input$source_select, collapse = "%27,%27"),"%27)"),"")
+
+
+types_filter <- ifelse(length(input$type_select) > 0, 
+                       paste0("%20AND%20%22REQUEST_TYPE%22%20IN%20(%27", paste(input$type_select, collapse = "%27,%27"),"%27)"),"")
 
